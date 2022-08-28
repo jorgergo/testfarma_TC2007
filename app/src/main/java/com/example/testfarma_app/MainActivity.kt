cabc2b8e9e38ee5c91251857d188516257a7670d
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2022-08-28 18:16:09 -0500

    Update main view
    Agregue los cambios de las imágenes y le puse los botones con texto (aún sin funcionar)

--- app/src/main/java/com/example/testfarma_app/MainActivity.kt
+++ app/src/main/java/com/example/testfarma_app/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.testfarma_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class MainActivity : AppCompatActivity() {
+
+    private val list = mutableListOf<CarouselItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+        list.add(CarouselItem(imageDrawable = R.drawable.imagen_ejemplouno))
+        list.add(CarouselItem(imageDrawable = R.drawable.imagen_ejemplodos))
+        carousel.addData(list)
+
     }
 }
