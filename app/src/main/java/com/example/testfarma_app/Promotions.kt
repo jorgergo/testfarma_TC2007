c8e272322d2672ee22f67d82a4932a27b301a2d7
Aderjarn <feacfabian@gmail.com>
2022-09-04 21:08:33 -0500

    Feat: Added class para promociones
    Fix: Made project adjustments.

--- app/src/main/java/com/example/testfarma_app/Promotions.kt
+++ app/src/main/java/com/example/testfarma_app/Promotions.kt
@@ -4,10 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.promo_layout.*
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class Promotions: AppCompatActivity() {
+
+    private val list = mutableListOf<CarouselItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.promo_layout)
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+        list.add(CarouselItem(imageDrawable = R.drawable.imagen_ejemplouno))
+        list.add(CarouselItem(imageDrawable = R.drawable.imagen_ejemplouno))
+        carousel.addData(list)
+
+        setContentView(R.layout.promo_layout)
+        val promoCarousel: ImageCarousel = findViewById(R.id.promo_carousel)
+        list.add(CarouselItem(imageDrawable = R.drawable.imagen_ejemplouno))
+        list.add(CarouselItem(imageDrawable = R.drawable.imagen_ejemplouno))
+        promoCarousel.addData(list)
+
         setContentView(R.layout.promo_layout)
 
         back.setOnClickListener {
