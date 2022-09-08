f48e810a49a7035200a554f5397117b82b8cf4ee
Aderjarn <feacfabian@gmail.com>
2022-09-07 22:58:34 -0500

    Feat: Added layout y class para mostrar facturas
    Fix: Made project adjustments.

---
+++ app/src/main/java/com/example/testfarma_app/ShowFacturas.kt
@@ -0,0 +1,25 @@
+package com.example.testfarma_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.promo_layout.*
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+
+class ShowFacturas: AppCompatActivity() {
+
+    private val list = mutableListOf<CarouselItem>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.show_facturas_layout)
+
+        back.setOnClickListener {
+            val intent = Intent(this, MainActivity_NotSignedIn::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}
