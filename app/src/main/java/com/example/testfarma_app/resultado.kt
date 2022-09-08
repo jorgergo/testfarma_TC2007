412fe5c5f1b5a68d5c5e1ce66a7918601aa51c60
fabian994 <a01367585@itesm.mx>
2022-09-08 17:56:46 -0500

    feat, temp changes to resultado screen
    Temporal changes to Resultado screen, proper development in pruebasFabG

---
+++ app/src/main/java/com/example/testfarma_app/resultado.kt
@@ -0,0 +1,11 @@
+package com.example.testfarma_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class resultado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
