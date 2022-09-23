b627dafeccf1f92b503b1ed93397220c4d697d7e
fabian994 <a01367585@itesm.mx>
2022-09-23 11:45:28 -0500

    fixed workspace

--- app/src/main/java/com/example/testfarma_app/resultado.kt
+++ app/src/main/java/com/example/testfarma_app/resultado.kt
@@ -2,10 +2,19 @@ package com.example.testfarma_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testfarma_app.adapterResultados.resultadosAdapter
+import com.example.testfarma_app.dataResultados.datasourceResultados
 
 class resultado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+        val myDataset = datasourceResultados().loadResultados()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_res)
+        //should work
+
+        recyclerView.adapter = resultadosAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
