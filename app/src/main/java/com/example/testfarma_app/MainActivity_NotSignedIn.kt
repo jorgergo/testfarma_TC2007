e08ab5082dbcac45955bf97d194f015a927dc3d4
fabian994 <a01367585@itesm.mx>
2022-09-29 16:31:32 -0500

    commented

--- app/src/main/java/com/example/testfarma_app/MainActivity_NotSignedIn.kt
+++ app/src/main/java/com/example/testfarma_app/MainActivity_NotSignedIn.kt
@@ -51,10 +51,10 @@ class MainActivity_NotSignedIn : AppCompatActivity() {
 
 
         //  ir a la pantalla de resultado
-
+/*
         buttonRes.setOnClickListener {
             startActivity(Intent(this, resultado::class.java))
-        }
+        }*/
 
 
     }
