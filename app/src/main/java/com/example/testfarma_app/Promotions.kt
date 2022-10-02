49c03b0c3482a2f3a25fe82704acb9cab6c0aa5a
Aderjarn <feacfabian@gmail.com>
2022-10-02 01:20:35 -0500

    Update Promotions.kt

--- app/src/main/java/com/example/testfarma_app/Promotions.kt
+++ app/src/main/java/com/example/testfarma_app/Promotions.kt
@@ -39,6 +39,11 @@ class Promotions: AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        perfil.setOnClickListener {
+            val intent = Intent( this, UserInfo::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
 }
