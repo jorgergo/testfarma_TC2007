339135d83a9c7d3d8f8888e306fa00435afe002e
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2022-10-04 17:35:40 -0500

    Registro
    El registro ya verifica un correo y contraseña con un regex

--- app/src/main/java/com/example/testfarma_app/SignUp.kt
+++ app/src/main/java/com/example/testfarma_app/SignUp.kt
@@ -6,18 +6,22 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.util.PatternsCompat
+import com.example.testfarma_app.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_reg.*
+import java.util.regex.Pattern
 
 class SignUp : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     val database = Firebase.database
     val ref = database.getReference("User")
     private lateinit var auth: FirebaseAuth
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reg)
 
@@ -30,35 +34,82 @@ class SignUp : AppCompatActivity() {
     }
 
     private fun setup() {
+
         auth =  Firebase.auth
         signUpBtn.setOnClickListener {
 
-            val espacioEmail = findViewById<EditText>(R.id.editTextEmail)
-            val espacioNombre = findViewById<EditText>(R.id.editTextName)
 
-            val email = espacioEmail.text.toString()
-            val nombre = espacioNombre.text.toString()
+            if (!validarEmail() && !validarContra()) {
+                showAlert()
+            } else {
+                val espacioEmail = findViewById<EditText>(R.id.editTextEmail)
+                val espacioNombre = findViewById<EditText>(R.id.editTextName)
 
-            if (email.isNotEmpty() && editTextPassword.text.isNotEmpty() &&
-                nombre.isNotEmpty()) {
-                val nombre = editTextName.text.toString()
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                    email,
-                    editTextPassword.text.toString()
-                ).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val user = auth.currentUser
-                        user?.let {
-                            regUsuario(user.uid)
+                val email = espacioEmail.text.toString()
+                val nombre = espacioNombre.text.toString()
+
+                if (email.isNotEmpty() && editTextPassword.text.isNotEmpty() &&
+                    nombre.isNotEmpty()) {
+
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                        email,
+                        editTextPassword.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val user = auth.currentUser
+                            user?.let {
+                                regUsuario(user.uid)
+                            }
+
+                            showHome()
+
+                        } else {
+                            showAlert()
                         }
-
-                        showHome()
-
-                    } else {
-                        showAlert()
                     }
                 }
             }
+        }
+    }
+
+    private fun validarEmail() : Boolean {
+        val espacioEmail = findViewById<EditText>(R.id.editTextEmail)
+        val email = espacioEmail.text.toString()
+        return if (email.isEmpty()) {
+            editTextEmail.error = "El campo del correo no puede estar vacío"
+            false
+        } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.error = "Escriba una dirección de correo válida"
+            false
+        } else {
+            editTextEmail.error = null
+            true
+        }
+    }
+
+    private fun validarContra() : Boolean {
+        val espacioContra = findViewById<EditText>(R.id.editTextPassword)
+        val contra = espacioContra.text.toString()
+        val passwordRegex = Pattern.compile(
+            "^" +
+                    "(?=.*[0-9])" +
+                    "(?=.*[a-z])" +
+                    "(?=.*[A-Z])" +
+                    "(?=.*[@#$%^&+=])" +
+                    "(?=\\S+$)" +
+                    ".{4,}" +
+                    "$"
+
+        )
+        return if (contra.isEmpty()) {
+            editTextPassword.error = "El campo del correo no puede estar vacío"
+            false
+        }else if (!passwordRegex.matcher(contra).matches()) {
+            editTextPassword.error = "Revise la contraseña que contenga todo lo que se pide"
+            false
+        } else {
+            editTextPassword.error = null
+            true
         }
     }
 
