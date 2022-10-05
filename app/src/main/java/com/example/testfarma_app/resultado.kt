28c2c07fce625cade5e26003ed1d0b5ddb0b59c8
fabian994 <a01367585@itesm.mx>
2022-10-05 18:08:20 -0500

    Feat, Added filtering by date to Resultados

--- app/src/main/java/com/example/testfarma_app/resultado.kt
+++ app/src/main/java/com/example/testfarma_app/resultado.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +14,9 @@ import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
+import kotlinx.android.synthetic.main.activity_resultado.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class resultado : AppCompatActivity() {
@@ -37,17 +41,19 @@ class resultado : AppCompatActivity() {
         histRecyclerView.layoutManager = GridLayoutManager(this, 2)
         histRecyclerView.setHasFixedSize(true)
 
+        archArrayListRes = arrayListOf<arch_resultado>()
         archArrayList = arrayListOf<arch_resultado>()
         getArchdata()
     }
 
 
-    fun download(f_name: String) {
+    fun download(f_name: String,t_files: String) {
         val storageReference = FirebaseStorage.getInstance().reference
-        val ref = storageReference.child("Resultados/$f_name")
+        val ref = storageReference.child("Resultados/$f_name.$t_files")
         ref.downloadUrl.addOnSuccessListener { uri ->
             val url = uri.toString()
-            downloadFiles(this@resultado, f_name, ".pdf", Environment.DIRECTORY_DOWNLOADS, url)
+            Toast.makeText(this@resultado, "Descargando: "+f_name+t_files, Toast.LENGTH_SHORT).show()
+            downloadFiles(this@resultado, f_name, t_files, Environment.DIRECTORY_DOWNLOADS, url)
         }.addOnFailureListener { }
     }
 
@@ -74,16 +80,50 @@ class resultado : AppCompatActivity() {
         dbref = FirebaseDatabase.getInstance().getReference("Resultados/u1")
         dbref.addValueEventListener(object : ValueEventListener{
 
+
             override fun onDataChange(snapshot: DataSnapshot) {
+                val sdf = SimpleDateFormat("dd-MM-yyyy")
+                var fechaBase = "18-01-2010"
+                var fechaArch = ""
+                var fechaRec: Date = sdf.parse(fechaBase) as Date
                 if (snapshot.exists()){
                     for(userSnapshot in snapshot.children){
                         val user = userSnapshot.getValue(arch_resultado::class.java)
                         archArrayList.add(user!!)
                         //var fechas = archArrayList.date.toString
+                        fechaArch = user.date.toString()
+                        var fechaArchivo: Date = sdf.parse(fechaArch) as Date
+
+                        var fechaRec: Date = sdf.parse(fechaBase) as Date
+                        var cmp = fechaRec.compareTo(fechaArchivo)
+                        Log.i("Estatus", "fecha $fechaArch");
+                        Log.i("Estatus", "cmp $cmp");
+
+                        if (cmp < 0){
+                            fechaBase = fechaArch
+                        }
+                    }
+                    Log.i("Estatus", "-----Fecha F $fechaBase");
+                    for(userSnapshot in snapshot.children) {
+                        val user2 = userSnapshot.getValue(arch_resultado::class.java)
+                        var fechaArch = user2?.date.toString()
+                        var fechaArchivo: Date = sdf.parse(fechaArch) as Date
+
+                        var fechaRec: Date = sdf.parse(fechaBase) as Date
+                        var cmp2 = fechaRec.compareTo(fechaArchivo)
+                        Log.i("Estatus", "Fecha F $fechaBase");
+                        Log.i("Estatus", "fecha $fechaArch");
+                        Log.i("Estatus", "cmp $cmp2");
+
+                        if (cmp2 == 0){
+                            archArrayListRes.add(user2!!)
+                        }
                     }
                     var adapter =resultadosAdapter(archArrayList)
-                    resRecyclerView.adapter = adapter
+                    var adapterRes =resultadosAdapter(archArrayListRes)
+                    resRecyclerView.adapter = adapterRes
                     histRecyclerView.adapter = adapter
+
                     adapter.setOnClickListener(object : resultadosAdapter.onItemClickListener{
                         override fun onItemClick(position: Int) {
                             //Toast.makeText(this@resultado, "clicked item no. "+ archArrayList.get(position).f_name.toString(),Toast.LENGTH_SHORT).show()
@@ -93,7 +133,24 @@ class resultado : AppCompatActivity() {
                             var strStringName = nombreFile.split(".").toTypedArray()
                             //Toast.makeText(this@resultado, "nombre archivo: "+nombreDarchivo+tipoDarchivo, Toast.LENGTH_SHORT).show()
                             //downloadFiles(nombreDarchivo,tipoDarchivo)
-                            download(nombreFile)
+                            var nomFile = strStringName[0]
+                            var tFile = strStringName[1]
+                            download(nomFile, tFile)
+                        }
+                    })
+
+                    adapterRes.setOnClickListener(object : resultadosAdapter.onItemClickListener{
+                        override fun onItemClick(position: Int) {
+                            //Toast.makeText(this@resultado, "clicked item no. "+ archArrayList.get(position).f_name.toString(),Toast.LENGTH_SHORT).show()
+
+                            var nombreFile2 = archArrayListRes.get(position).f_name.toString()
+                            //Toast.makeText(this@resultado, "clicked item : "+ nombreFile,Toast.LENGTH_SHORT).show()
+                            var strStringName2 = nombreFile2.split(".").toTypedArray()
+                            //Toast.makeText(this@resultado, "nombre archivo: "+nombreDarchivo+tipoDarchivo, Toast.LENGTH_SHORT).show()
+                            //downloadFiles(nombreDarchivo,tipoDarchivo)
+                            var nomFile2 = strStringName2[0]
+                            var tFile2 = strStringName2[1]
+                            download(nomFile2, tFile2)
                         }
                     })
                 }
@@ -101,7 +158,7 @@ class resultado : AppCompatActivity() {
 
 
             override fun onCancelled(error: DatabaseError) {
-                Log.i("Estatus", "No jalo el archdata");
+                Log.i("Estatus", "Error de conexion");
             }
         }
         )
